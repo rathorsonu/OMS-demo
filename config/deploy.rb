66ba6b8de167c9c54ad66501bc605049bffc3beb
rathorsonu <sunnysonurathor2016@gmail.com>
2024-06-14 14:06:33 +0530

    add deployee file

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ set :pty, true
 
 set :branch, 'main'
 set :linked_files, %w{config/master.key config/database.yml}
+append :linked_files, 'config/master.key'
+
 set :rails_env, 'production'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # Default branch is :master
