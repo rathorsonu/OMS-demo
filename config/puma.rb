114358bce67d3ea11152d787f1da99b7ef5c3abf
rathorsonu <sunnysonurathor2016@gmail.com>
2024-06-17 12:32:40 +0530

    puma .rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,51 +1,39 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+# Fetching the maximum and minimum threads count from environment variables
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+# Setting the port for Puma to listen on
+port ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+# Setting the environment for Puma
+environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Specifies the `pidfile` that Puma will use.
+# Setting the pidfile for Puma
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
+# Setting the number of workers (commented out as it's optional)
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
+# Preloading the app (commented out as it's optional)
 # preload_app!
-# Allow puma to be restarted by `rails restart` command.
+
+# Running Puma as a daemon
 daemonize true
-# PID and state file locations
+
+# Setting the directory and rackup file
 directory '/home/deploy/oms_demo/current'
 rackup "/home/deploy/oms_demo/current/config.ru"
+
+# Setting the environment explicitly (even though it's already set above)
 environment 'production'
 
-tag ''
-
+# Setting the pidfile and state file locations
 pidfile "/home/deploy/oms_demo/shared/tmp/pids/puma.pid"
 state_path "/home/deploy/oms_demo/shared/tmp/pids/puma.state"
+
+# Redirecting stdout and stderr logs
 stdout_redirect "/home/deploy/oms_demo/shared/log/puma.stdout.log", "/home/deploy/oms_demo/shared/log/puma.stderr.log", true
 
+# Allowing puma to be restarted by `rails restart` command
 plugin :tmp_restart
-
-
